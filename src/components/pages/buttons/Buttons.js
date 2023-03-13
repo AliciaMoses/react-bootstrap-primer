16d44037d74ffdd4abb8654f50ecff0402ebec52
Alicia Moses <aliciamoses.dev@gmail.com>
2023-03-13 20:50:47 +0000

    Added syntaxhighlighter for code snippet rendering Added demo button components and main page content

--- src/components/pages/buttons/Buttons.js
+++ src/components/pages/buttons/Buttons.js
@@ -1,10 +1,41 @@
 import React from "react";
 import Layout from "../../base-components/layout/Layout";
+import btnCode from "./btnCode";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Container } from "react-bootstrap";
+import SolidButtons from "../../demo-components/button/Solid";
+import OutlineButton from "../../demo-components/button/Outline";
 
 const Buttons = () => {
   return (
     <Layout>
-      <h1>Buttons</h1>
+      <h1 className="display-4 text-center">Buttons</h1>
+
+      <Container style={{ maxWidth: "750px" }}>
+        <p className="lead text-center">
+          Buttons are one of the most important components in a user interface,
+          as they allow users to interact with the application and trigger
+          various events. React-Bootstrap provides a flexible Button component
+          that can be customized with various styles and functionalities,
+          allowing developers to create interactive elements that align with the
+          design and functionality of their application.
+        </p>
+        <br></br>
+        <h1 class="display-6 text-center">Importing Buttons</h1>
+        <SyntaxHighlighter language="javascript" style={atomDark}>
+          {btnCode}
+        </SyntaxHighlighter>
+        <br></br>
+        <p className="lead">Predefined button styles include:</p>
+        <SolidButtons />
+        <br></br>
+        <br></br>
+        <p className="lead">
+          For outline buttons, variants are defined as outline-*
+        </p>
+        <OutlineButton />
+      </Container>
     </Layout>
   );
 };
---
+++ src/components/pages/buttons/btnCode.js
@@ -0,0 +1,16 @@
+const btnCode = `
+  // Import the button component
+  import { Button } from 'react-bootstrap';
+
+  // Define a component
+  const MyButton = () => {
+    return <Button>Click me!</Button>;
+  };
+
+  // Set the Bootstrap theme for the button
+  const SuccessButton = () => {
+    return <Button variant="success">Click me!</Button>;
+  };  
+`;
+
+export default btnCode;
